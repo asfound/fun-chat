98e46f3edbd518dda2545d02d35eda6a079486dc
asfound <fffound27@gmail.com>
2025-07-24 20:22:43 +0200

    chore: remove nested dir and update server url

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://127.0.0.1:4000';
+export const BASE_URL = 'http://fun-chat-app-cj829.ondigitalocean.app:80';
 
 export const APP_NAME = 'Fun Chat';
 export const APP_YEAR = '2025';
